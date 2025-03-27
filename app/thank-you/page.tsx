e7b0c69799c54b4296ed86360ee81fa97fbafe05
jordanobl <jordan.williams@codemith.io>
2025-03-27 00:33:39 -0400

    thankyou page link to changed to href

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -6,7 +6,7 @@ export default function ThankYouPage() {
         <h1 className="text-3xl font-bold text-teal-400">Thank You!</h1>
         <p className="text-gray-700 mt-2">Your payment was successful.</p>
         <p className="text-gray-500">We have sent a confirmation email with the details.</p>
-        <Link to='/' className="mt-4 inline-block px-6 py-2 text-white bg-primary rounded-lg hover:bg-secondary hover:text-primary transition">
+        <Link href="/" className="mt-4 inline-block px-6 py-2 text-white bg-primary rounded-lg hover:bg-secondary hover:text-primary transition">
           Back to Homepage
         </Link>
       </div>
